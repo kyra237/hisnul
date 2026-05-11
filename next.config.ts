dfd312cf3c9168ce463a4f310d51cddeebb1b501
Rizky Fauzi Ilmi <rizkyfauziilmi@gmail.com>
2026-05-11 19:45:25 +0700

    change next config for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
+};
 
 export default nextConfig;
